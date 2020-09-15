281e8468a83136b79180373eeb7c3fa7673aa15a
ekaterinaborodavka <ekaterinaborodavka3@gmail>
2020-09-15 21:05:32 +0300

    add about

---
+++ src/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './About.css'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1 className='Title'>Toys Warehouse</h1>
+            <h2 className='About_Title'>About</h2>
+            <div className='About_Content'>
+                Page about app's author
+            </div>
+        </React.Fragment>
+    )
+}
